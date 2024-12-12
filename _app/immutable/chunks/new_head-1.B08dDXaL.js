069a750e9e6a7883d7fb098e681f5a9cb8694eec
s1m0n-ai <s4c.semen.s@gmail.com>
2024-12-12 09:01:15 +0300

    Add files via upload

---
+++ _app/immutable/chunks/new_head-1.B08dDXaL.js
@@ -0,0 +1,1 @@
+const e=""+new URL("../assets/new_head-1.PMEmSifB.png",import.meta.url).href,s=""+new URL("../assets/new_head-1.jlpMyEle.jpg",import.meta.url).href;export{s as g,e as h};
---
+++ _app/immutable/entry/start.0_Pp0uX1.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DLFYdScg.js";export{t as start};
